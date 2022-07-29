f0e07e06ca5df9adbe7d402ed6bfa32748cb1625
jezburg <darhan.kassymbekov@gmail.com>
2022-07-29 12:57:09 +0600

    Thirt commit

--- PrintLinkedList.py
+++ PrintLinkedList.py
@@ -1,3 +1,32 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = SinglyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+
+
+        self.tail = node
+
 # Complete the printLinkedList function below.
 
 #
@@ -8,16 +37,12 @@
 #     SinglyLinkedListNode next
 #
 #
-class SinglyLinkedList:
-    def __init__ (self):
-        self.head = None
-
 def printLinkedList(head):
     itr = head
     while itr:
         print(itr.data)
         itr = itr.next 
-    
+
 if __name__ == '__main__':
     llist_count = int(input())
 
