37828270500b77c1889ec99d131c64014ee2cde0
jezburg <darhan.kassymbekov@gmail.com>
2022-07-29 15:06:37 +0600

    Solution

---
+++ deleteNode.py
@@ -0,0 +1,103 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = SinglyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+
+
+        self.tail = node
+
+def print_singly_linked_list(node, sep, fptr):
+    while node:
+        fptr.write(str(node.data))
+
+        node = node.next
+
+        if node:
+            fptr.write(sep)
+
+#
+# Complete the 'deleteNode' function below.
+#
+# The function is expected to return an INTEGER_SINGLY_LINKED_LIST.
+# The function accepts following parameters:
+#  1. INTEGER_SINGLY_LINKED_LIST llist
+#  2. INTEGER position
+#
+
+#
+# For your reference:
+#
+# SinglyLinkedListNode:
+#     int data
+#     SinglyLinkedListNode next
+#
+#
+
+def deleteNode(llist, position):
+    if llist is None:
+        return
+    
+    if position == 0:
+        return llist.next
+    
+    itr = llist
+    len = 0
+    while itr:
+        itr = itr.next
+        len += 1
+
+    if position < 0 or position > len:
+        return
+    
+        
+        
+    itr = llist
+    counter = 0
+    while itr.next:
+        if counter == position - 1:
+            itr.next = itr.next.next
+            return llist
+        itr = itr.next
+        counter += 1
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    llist_count = int(input())
+
+    llist = SinglyLinkedList()
+
+    for _ in range(llist_count):
+        llist_item = int(input())
+        llist.insert_node(llist_item)
+
+    position = int(input())
+
+    llist1 = deleteNode(llist.head, position)
+
+    print_singly_linked_list(llist1, ' ', fptr)
+    fptr.write('\n')
+
+    fptr.close()
